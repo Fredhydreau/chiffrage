073d157759ca9aa56220c60ac11c40b1d31e9c69
Fredhydreau <contact@hydreau-forage.fr>
2026-09-24 11:59:44 +0200

    Add Firebase configuration to bofig.js

---
+++ bofig.js
@@ -0,0 +1,8 @@
+window.HYDREAU_FIREBASE = {
+  apiKey: "AIzaSyBk7wbD2LRjKoJstHbmDZQYzro92p0gRoA",
+  authDomain: "hydreau-chiffrage.firebaseapp.com",
+  projectId: "hydreau-chiffrage",
+  storageBucket: "hydreau-chiffrage.firebasestorage.app",
+  messagingSenderId: "708848053989",
+  appId: "1:708848053989:web:b588091d7f61911d75ee0c"
+};
